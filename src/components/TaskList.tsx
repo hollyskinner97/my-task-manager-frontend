d3fe0d7313c3be5e2dce8f697cb91446ff2ffca7
Holly Skinner <hollyskinner97@gmail.com>
2025-04-07 15:17:52 +0100

    simplify TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from "react";
 import { Task } from "../types/Task";
-import { API_URL } from "../config";
 import TaskItem from "./TaskItem";
 
-const TaskList: React.FC = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [loading, setLoading] = useState(true);
+interface TaskListProps {
+  tasks: Task[];
+}
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data: Task[]) => {
-        setTasks(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Failed to fetch tasks", err);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading tasks...</p>;
+const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
   if (!tasks.length) return <p>No tasks yet...</p>;
 
   return (
