e692881703f61030a39c045f36a29654ca091468
Holly Skinner <hollyskinner97@gmail.com>
2025-04-11 21:32:30 +0100

    add util fn to use local timezone for dates

---
+++ src/components/utils/toLocalDatetimeString.tsx
@@ -0,0 +1,5 @@
+export default function toLocalDatetimeString(date: Date) {
+  const offset = date.getTimezoneOffset();
+  const local = new Date(date.getTime() - offset * 60 * 1000);
+  return local.toISOString().slice(0, 16);
+}
